0793066266ecda3ae09d542cb931e72333bf39b4
naazem <sinanaazem@gmail.com>
2022-01-23 22:38:13 +0330

    compleat template telegram bot

---
+++ src/football_bot/bot.py
@@ -0,0 +1,27 @@
+import os
+
+import telebot
+from loguru import logger
+
+
+
+telegram_bot_token = os.environ['TELEGRAMBOT_TOKEN']
+
+class Bot():
+
+    def __init__(self):
+        self.bot = telebot.TeleBot(telegram_bot_token)
+        logger.info("start bot...")
+    def run(self):
+
+        @self.bot.message_handler(commands=['start', 'help'])
+        def send_welcome(message):
+            self.bot.reply_to(message, "Howdy, how are you doing?",reply_markup=create_keybord('Bitcoin'))
+
+
+
+        self.bot.infinity_polling()
+
+if __name__ == "__main__":
+    obj = Bot()
+    obj.run()
